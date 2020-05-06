272d4c397237a6254998ed715ee2f0a8c669b32b
Filipe Andrade <filipeandrade.dev@gmail.com>
2020-05-06 14:44:21 -0300

    Updates data model

--- dynamodb/data-model.js
+++ dynamodb/data-model.js
@@ -5,8 +5,8 @@ const collections = [
 ]
 
 payload_metadata = [{
-    yyyy_mm_dd_hh_mm_ss: '2020|04|25|19|33|32',
-    latitude_longitude: '-22.9377794|-47.0303715',
+    yyyy_mm_dd_hh_mm_ss: "2020|04|25|19|33|32",
+    latitude_longitude: "-22.9377794|-47.0303715",
     user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36 OPR/67.0.3575.137",
     origin: "https://www.site.com.br",
 }]
