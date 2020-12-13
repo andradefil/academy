e54187fc38442ee47eb3cdf09491540d692981a4
Filipe Andrade <filipeandrade.dev@gmail.com>
2020-12-12 23:42:35 -0300

    Adds appollo-tutorial basic project

---
+++ apollo-tutorial/final/client/src/pages/__generated__/login.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: Login
+// ====================================================
+
+export interface login_login {
+  __typename: "User";
+  id: string;
+  token: string | null;
+}
+
+export interface login {
+  login: login_login;
+}
+
+export interface loginVariables {
+  email: string;
+}
