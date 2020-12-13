9a092759372002fe8aac3ad901f0b867af20ba36
Filipe Andrade <filipeandrade.dev@gmail.com>
2020-12-13 01:06:00 -0300

    Adds query resolvers

--- apollo-tutorial/start/server/src/index.js
+++ apollo-tutorial/start/server/src/index.js
@@ -1,7 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers')
+const { createStore } = require('./utils')
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch')
+const UserAPI = require('./datasources/user')
+
+const store = createStore();
+
+const server = new ApolloServer({
+   typeDefs,
+   resolvers,
+   dataSources: () => ({
+     launchAPI: new LaunchAPI(),
+     userAPI: new UserAPI( { store })
+   }) 
+});
+
 server.listen().then(() => {
   console.log(`
     Server is running!
--- apollo-tutorial/start/server/src/schema.js
+++ apollo-tutorial/start/server/src/schema.js
@@ -40,9 +40,29 @@ const typeDefs = gql`
   }
 
   type Query {
-   launches: [Launch]!
+   launches( # replace the current launches query with this one.
+     """
+     The number of results to show. Must be >= 1. Default = 20
+     """
+     pageSize: Int
+     """
+     If you add a cursor here, it will only return results _after_ this cursor
+     """
+     after: String
+   ): LaunchConnection!
    launch(id: ID!): Launch
    me: User
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   type Mutation {
