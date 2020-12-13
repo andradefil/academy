3752956565bc1168f9b986d008db66c6df95f988
Filipe Andrade <filipeandrade.dev@gmail.com>
2020-12-13 01:23:36 -0300

    Adds mutations resolvers

--- apollo-tutorial/start/server/src/index.js
+++ apollo-tutorial/start/server/src/index.js
@@ -7,8 +7,21 @@ const LaunchAPI = require('./datasources/launch')
 const UserAPI = require('./datasources/user')
 
 const store = createStore();
+const isEmail = require('isemail')
+
+async function loginFunction({ req }) {
+  // simple auth check on every request
+  const auth = req.headers && req.headers.authorization || '';
+  const email = Buffer.from(auth, 'base64').toString('ascii');
+  if (!isEmail.validate(email)) return { user: null };
+  // find a user by their email
+  const users = await store.users.findOrCreate({ where: { email } });
+  const user = users && users[0] || null;
+  return { user: { ...user.dataValues } };
+}
 
 const server = new ApolloServer({
+  context: loginFunction,
    typeDefs,
    resolvers,
    dataSources: () => ({
--- apollo-tutorial/start/server/src/resolvers.js
+++ apollo-tutorial/start/server/src/resolvers.js
@@ -1,6 +1,49 @@
 const { paginateResults } = require('./utils') 
 
 module.exports = {
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) {
+        user.token = Buffer.from(email).toString('base64');
+        return user;
+      }
+    },
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+
+      return {
+        success: results && results.length === launchIds.length,
+        message:
+          results.length === launchIds.length
+            ? 'trips booked successfully'
+            : `the following launches couldn't be booked: ${launchIds.filter(
+                id => !results.includes(id),
+              )}`,
+        launches,
+      };
+    },
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result)
+        return {
+          success: false,
+          message: 'failed to cancel trip',
+        };
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch],
+      };
+    },
+  },
+
   Query: {
      launches: async (_, { pageSize = 20, after }, { dataSources }) => {
       const allLaunches = await dataSources.launchAPI.getAllLaunches();
