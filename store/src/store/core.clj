7aa74110b934a8432cd70f5c3c02871ad8c50f30
Filipe Andrade <filipeandrade.dev@gmail.com>
2021-07-13 19:02:23 -0300

    Learning more clojure

---
+++ store/src/store/core.clj
@@ -0,0 +1,2 @@
+(ns store.core)
+
---
+++ store/src/store/lesson1.clj
@@ -0,0 +1,51 @@
+(ns store.lesson1)
+
+;["daniel" "willian" "carl" "paul" "anna"]
+(map println ["daniel" "willian" "carl" "paul" "anna"])
+
+(println (first ["daniel" "willian" "carl" "paul" "anna"]))
+
+(println (rest ["daniel" "willian" "carl" "paul" "anna"]))
+
+(println (next ["daniel" "willian" "carl" "paul" "anna"]))
+
+(println (rest []))
+
+(println (next []))
+
+(println (seq []))
+(println (seq [1 2 3 17]))
+
+(println "\n\nMY MAP")
+(defn my-map
+  [function sequence]
+  (let [first-element (first sequence)]
+    (if first-element
+      (function first-element)
+      (my-map function (rest sequence))))
+  )
+
+(println "\n\nMY MAP")
+(defn my-map
+  [function sequence]
+  (let [first-element (first sequence)]
+    (if first-element
+      (do
+        (function first-element)
+        (my-map function (rest sequence)))))
+  )
+
+(println "\n\nMY MAP STOPPING ONLY FOR NULL")
+(defn my-map
+  [function sequence]
+  (let [first-element (first sequence)]
+    (if (not (nil? first-element))
+      (do
+        (function first-element)
+        (my-map function (rest sequence)))))
+  )
+
+;(my-map println ["daniel" "willian" "carl" "paul" "anna"])
+(my-map println ["daniel" false "carl" "paul" "anna"])
+(my-map println [])
+(my-map println nil)
