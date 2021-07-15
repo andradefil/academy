ead9499565b010110d85bc63b70f10ae88796671
Filipe Andrade <filipeandrade.dev@gmail.com>
2021-07-14 21:43:49 -0300

    Finished lesson 1

--- hospital/src/hospital/lesson1.clj
+++ hospital/src/hospital/lesson1.clj
@@ -3,6 +3,28 @@
   (:require [hospital.model :as h.model])
   (:require [hospital.logic :as h.logic]))
 
+(defn terrible-arrive-at
+  [person]
+  (def hospital (h.logic/arrived-at-with-pauses hospital :g-queue person))
+  (println "After inserting" person)
+  )
+
+(defn simulates-a-day-in-parallel
+  []
+  (def hospital (h.model/new-hospital))
+  (.start (Thread. (fn [] (terrible-arrive-at "111"))))
+  (.start (Thread. (fn [] (terrible-arrive-at "222"))))
+  (.start (Thread. (fn [] (terrible-arrive-at "333"))))
+  (.start (Thread. (fn [] (terrible-arrive-at "444"))))
+  (.start (Thread. (fn [] (terrible-arrive-at "555"))))
+  (.start (Thread. (fn [] (terrible-arrive-at "666"))))
+
+  (.start (Thread. (fn [] (Thread/sleep 4000)
+                     (pprint hospital))))
+  )
+
+(simulates-a-day-in-parallel)
+
 (defn simulates-a-day
   []
   (def hospital (h.model/new-hospital))
@@ -18,7 +40,14 @@
 
   (def hospital (h.logic/was-attended-to hospital :lab1))
   (def hospital (h.logic/was-attended-to hospital :g-queue))
+
+
+  (def hospital (h.logic/arrived-at hospital :g-queue "666"))
+  (def hospital (h.logic/arrived-at hospital :g-queue "777"))
+  (def hospital (h.logic/arrived-at hospital :g-queue "888"))
+  (pprint hospital)
+  (def hospital (h.logic/arrived-at hospital :g-queue "999"))
   (pprint hospital)
   )
 
-(simulates-a-day)
+;(simulates-a-day)
--- hospital/src/hospital/logic.clj
+++ hospital/src/hospital/logic.clj
@@ -1,9 +1,28 @@
 (ns hospital.logic
   (:use [clojure pprint]))
 
+(defn fits-in-queue?
+  [hospital department]
+  (-> hospital
+      (get,,, department)
+      count ,,,
+      (<,,, 5)
+      ))
+
 (defn arrived-at
   [hospital department person]
-  (update hospital department conj person))
+    (if (fits-in-queue? hospital department)
+      (update hospital department conj person)
+      (throw (ex-info "The queue is full" {:trying-to-add person})))
+    )
+
+(defn arrived-at-with-pauses
+  [hospital department person]
+  (if (fits-in-queue? hospital department)
+    (do (Thread/sleep (* (rand) 2000))
+        (update hospital department conj person))
+    (throw (ex-info "The queue is full" {:trying-to-add person})))
+  )
 
 (defn was-attended-to [hospital department]
   (update hospital department pop))
