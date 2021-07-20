f9a648b6ca3a048817bd480c5f1ca99aec07cf9e
Filipe Andrade <filipeandrade.dev@gmail.com>
2021-07-20 10:57:34 -0300

    finished up lesson 2

---
+++ ecommerce2/src/ecommerce2/lesson2.clj
@@ -0,0 +1,33 @@
+(ns ecommerce2.lesson2
+  (:use [clojure.pprint])
+  (:require [datomic.api :as d]
+            [ecommerce2.db :as e.db]
+            [ecommerce2.model :as e.model]))
+
+(def conn (e.db/open-connection))
+(d/transact conn e.db/schema)
+
+(def computer
+  (e.model/new-product (e.model/uuid) "Expensive computer" "/expensive-computer" 35800.10M))
+(def fancy-mobile
+  (e.model/new-product "Fancy Mobile" "/fancy-mobile" 8882M))
+(def cheap-mobile
+  (e.model/new-product "Cheap Mobile" "/cheap-mobile" 000.9M))
+(def calculator {:product/name "4-operations Calculator"})
+
+(d/transact conn [computer fancy-mobile cheap-mobile calculator])
+
+(def another-cheap-mobile (e.model/new-product
+                            (cheap-mobile :product/id)
+                            "ANOTHER ONE"
+                            "/another-one"
+                            0.00001M))
+
+(pprint (e.db/all-products (d/db conn)))
+(pprint (another-cheap-mobile :product/id))
+(d/transact conn [another-cheap-mobile])
+
+(def products (e.db/all-products (d/db conn)))
+(pprint products)
+
+;(e.db/delete-database)
